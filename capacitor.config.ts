8c589315274c3f370c05d93b7fe2265e1f07d22a
ofishea <fishflowbean@gmail.com>
2022-10-10 00:21:22 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'plitomean',
+  appName: 'Wallet App',
   webDir: 'www',
   bundledWebRuntime: false
 };
