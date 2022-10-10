97f35155d463f1c96d3d158e58c0c416b332678f
ofishea <fishflowbean@gmail.com>
2022-10-10 01:11:32 +0100

    Update fiat.page.ts

--- src/app/wallet/fiat/fiat.page.ts
+++ src/app/wallet/fiat/fiat.page.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController, ModalController, IonRouterOutlet } from '@ionic/angular';
-import { AccountService, WalletService } from '@app/_services';
-import { Account, Wallet } from '@app/_models';
 import { first } from 'rxjs/operators';
 import { HistoryPage } from '../../modals/history/history.page';
 
@@ -12,13 +10,8 @@ import { HistoryPage } from '../../modals/history/history.page';
   styleUrls: ['./fiat.page.scss'],
 })
 export class FiatPage implements OnInit {
-
-  account: Account;
-  wallet: Wallet[];
   
   constructor(
-    private accountService: AccountService,
-    private walletService: WalletService,
     private navCtrl: NavController,
     private router: Router,
     public routerOutlet: IonRouterOutlet,
@@ -26,10 +19,6 @@ export class FiatPage implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.accountService.account.subscribe(x => this.account = x);
-    this.walletService.getById('uid')
-    .pipe(first())
-    .subscribe(wallet => this.wallet = wallet);
   }
 
   dismiss() {
@@ -40,13 +29,10 @@ export class FiatPage implements OnInit {
     });
   }
  
-  async historyModal(uid: string) {
+  async historyModal() {
     const modal = await this.modalController.create({
       backdropDismiss:true,
       component: HistoryPage,
-      componentProps: {
-         uid: uid,
-      },
       cssClass: 'history-class',
       mode:'ios',
       swipeToClose:true,
