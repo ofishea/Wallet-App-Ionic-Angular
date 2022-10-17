8ea0ab2b4fbf0bc5c17a561e2edae8d661cfc55d
ofishea <fishflowbean@gmail.com>
2022-10-17 09:12:31 +0100

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Wallet App',
+  appName: 'Wallet-App-Ionic-Angular',
   webDir: 'www',
   bundledWebRuntime: false
 };
