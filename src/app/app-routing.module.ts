68e3c35a24a6a56db9f490525d5a46c51c1ce06e
ofishea <fishflowbean@gmail.com>
2022-10-18 21:52:57 +0100

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,14 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./wallet/wallet.module').then( m => m.WalletPageModule)
+  },
+  {
+    path: 'wallet',
+    loadChildren: () => import('./wallet/wallet.module').then( m => m.WalletPageModule)
+  },
+  {
     path: 'wallet/fiat/deposit/preview',
     loadChildren: () => import('./wallet/fiat/deposit/preview/preview.module').then( m => m.PreviewPageModule)
   },
@@ -13,10 +21,6 @@ const routes: Routes = [
   {
     path: 'wallet/fiat/withdraw',
     loadChildren: () => import('./wallet/fiat/withdraw/withdraw.module').then( m => m.WithdrawPageModule)
-  },
-  {
-    path: 'success',
-    loadChildren: () => import('./modals/success/success.module').then( m => m.SuccessPageModule)
   },
   // otherwise redirect to home
   { path: '**', redirectTo: 'wallet' }
