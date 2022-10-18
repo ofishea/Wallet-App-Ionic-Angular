775298a76a32b93cbe629ab5fff9f627d29e93ae
ofishea <fishflowbean@gmail.com>
2022-10-18 22:11:40 +0100

    Add files via upload

--- src/app/wallet/crypto-details/historymodal/historymodal.page.ts
+++ src/app/wallet/crypto-details/historymodal/historymodal.page.ts
@@ -35,19 +35,6 @@ export class HistorymodalPage implements OnInit {
               "hash": "386882cbd15944dbae436a92940e56f2c502d4584ab91c211da854818949f930"
             },
             {
-              "id": 2,
-              "name": "Bitcoin Received",
-              "slug": "receive",
-              "to": "13qPsUSJjw4dp1zv8MkrWiZvgqtbrTVYmr",
-              "from": "13qPsUSJjw4dp1zv8MkrWiZvgqtbrTVYmr",
-              "amount": "0.00382",
-              "amountngn": "NGN 33,471.75",
-              "date": "May 02 22",
-              "time": "4:32 PM",
-              "fees": "0",
-              "hash": "386882cbd15944dbae436a92940e56f2c502d4584ab91c211da854818949f930"
-            },
-            {
               "id": 3,
               "name": "Bitcoin Swap",
               "slug": "swap",
